f9453128383d041c3ced216e6a89f92948a622c3
Rafael Camargo <hello@rafaelcamargo.com>
2021-02-20 16:21:27 -0300

    feat: create feature card list

---
+++ src/scripts/base/components/feature-card-list/feature-card-list.js
@@ -0,0 +1,11 @@
+import '@styles/feature-card-list.styl';
+import { tcFeatureCard } from '@scripts/base/components/feature-card/feature-card';
+import template from './feature-card-list.html';
+
+export const tcFeatureCardList = {
+  name: 'tc-feature-card-list',
+  components: {
+    tcFeatureCard
+  },
+  template
+};
