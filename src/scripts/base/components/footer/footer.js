c18d3e9d15666b5513fcde94c89de4c44b0846e1
Rafael Camargo <hello@rafaelcamargo.com>
2021-03-07 09:25:49 -0300

    feat: improve layout

--- src/scripts/base/components/footer/footer.js
+++ src/scripts/base/components/footer/footer.js
@@ -1,7 +1,11 @@
 import '@styles/footer.styl';
+import { tcLink } from '@scripts/base/components/link/link';
 import template from './footer.html';
 
 export const tcFooter = {
   name: 'tc-footer',
+  components: {
+    tcLink
+  },
   template
 };
