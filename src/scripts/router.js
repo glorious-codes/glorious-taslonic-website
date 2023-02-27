05cf9853470c8a3c80abe06325d3b114a9a1eec4
Rafael Camargo <hello@rafaelcamargo.com>
2023-02-26 23:16:01 -0300

    chore: split bundle code by routes

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,35 +1,14 @@
 import VueRouter from 'vue-router';
 import routes from '@scripts/routes';
-import home from '@scripts/home/views/home';
-import installation from '@scripts/installation/views/installation';
-import react from '@scripts/usage/views/react/react';
-import vue from '@scripts/usage/views/vue/vue';
-import fundamentals from '@scripts/fundamentals/views/fundamentals';
-import principles from '@scripts/principles/views/principles';
 
 function buildRouter(){
-  const views = getViews();
   return new VueRouter({
-    routes: routes.map(route => {
-      route.component = views[route.name];
-      return route;
-    }),
+    routes,
     mode: 'history',
     scrollBehavior () {
       return { x: 0, y: 0 };
     }
   });
-}
-
-function getViews(){
-  return {
-    home,
-    installation,
-    vue,
-    react,
-    fundamentals,
-    principles
-  };
 }
 
 export const router = buildRouter();
--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -2,29 +2,35 @@ module.exports = [
   {
     path: '/',
     name: 'home',
+    component: () => import('./home/views/home')
   },
   {
     path: '/installation',
     name: 'installation',
+    component: () => import('./installation/views/installation'),
     shouldShowInMenu: true
   },
   {
     path: '/vue',
     name: 'vue',
+    component: () => import('./usage/views/vue/vue'),
     shouldShowInMenu: true
   },
   {
     path: '/react',
     name: 'react',
+    component: () => import('./usage/views/react/react'),
     shouldShowInMenu: true
   },
   {
     path: '/fundamentals',
     name: 'fundamentals',
+    component: () => import('./fundamentals/views/fundamentals'),
     shouldShowInMenu: true
   },
   {
     path: '/principles',
-    name: 'principles'
+    name: 'principles',
+    component: () => import('./principles/views/principles')
   }
 ];
